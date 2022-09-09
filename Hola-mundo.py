0340bc4bbdbc76db1e72afe7ee2fe86fd96ffb08
aayalac <aayalac@misena.edu.co>
2022-09-09 18:55:54 -0500

    Se mejoro el mensaje de bienvenida

--- Hola-mundo.py
+++ Hola-mundo.py
@@ -1,12 +1,9 @@
-saludo = input("Bienvenidos al programa de registro, ingrese los datos solicitados")
+print("Bienvenidos al programa de registro, ingrese los datos solicitados")
 
 nombre = input("Ingrese nombre: ")
 
 apellido = input("Ingrese apellido: ")
 
-print(saludo)
-
 #registro de usuario
 
-print ("Su nombre es: "+ nombre)
-print ("Su apellido es: "+ apellido)
+print("Bienvenid@ " + nombre +" "+ apellido)
