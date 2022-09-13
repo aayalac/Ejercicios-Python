3639e82ab9671276e75bb28c8e0bb55c1ec9f4ec
aayalac <aayalac@misena.edu.co>
2022-09-12 21:08:57 -0500

    Se agregan ejercicios

--- Ejericio-ciclo-2.py
+++ Ejericio-ciclo-2.py
@@ -1,8 +1,8 @@
 numero = int(input("Introduzca un numero "))
 contador = 0
-print("los divisores de ",numero)
-for divisor in range(1,numero+1):
-    if (numero % divisor) == 0 :
-        print(divisor,"es divisor")
+print("los divisores de ", numero)
+for divisor in range(1, numero + 1):
+    if (numero % divisor) == 0:
+        print(divisor, "es divisor")
         contador += 1
-print("el numero ",numero," tiene ",contador," divisores")
+print("el numero ", numero, " tiene ", contador, " divisores")
